daa994f25c4e43b7c3ccdc4b99e1b206ebc2d097
gabrielcho <gabrielarango15@gmail.com>
2022-05-04 18:15:41 -0500

    First push

---
+++ lambda/test-amp-website.js
@@ -0,0 +1,88 @@
+const amphtmlValidator = require('amphtml-validator');
+const got = require('got');
+const URL = require('url').URL;
+const validUrl = require('valid-url');
+
+
+/**
+ * Validates an AMP Website by getting the source code and passing it to the local NodeJS Validator API
+ * @param  {String} url A string containing a valid URL of a web
+ * @return {JSON} response A JSON forming the response to be delivered by the handler
+ */
+async function validateAMPWebsite(url){
+    let res;
+    let source;
+    try {
+        res = await got(url, {
+        timeout: {
+            request: 11000
+        }});
+        source = res.body;
+    } catch (err) {
+        let response = {
+            statusCode: undefined,
+            body: undefined
+        }
+        switch (err.code) { 
+            case "ENOTFOUND":
+                response.statusCode = 400;
+                response.body = JSON.stringify({testResult: "Could not reach targeturl"});
+                return response;
+            case "ETIMEDOUT":
+                response.statusCode = 504;
+                response.body = JSON.stringify({testResult: "Website timed out"})
+                return response 
+        }
+                
+    }
+
+
+    try {
+        const validator = await amphtmlValidator.getInstance();
+        let result = validator.validateString(source);
+
+        let response = {
+            statusCode: 200,
+            body: JSON.stringify({testResult: {status: result.status, errors:result.errors}})
+        };
+        return response;
+    } catch (err) {
+        let response = {
+            statusCode: 400,
+            body: JSON.stringify({source: source,  testResult: "Something happened with the validator"})
+        };
+        return response;
+    }
+   
+
+  }
+
+exports.testAMPWebsite = async (event) => {
+    let targeturl = event.queryStringParameters.targeturl;
+
+    let response = {
+        statusCode: null,
+        body: null
+    };
+
+    if (event.requestContext.http.method !== 'GET') {
+        throw new Error(`This function only accepts GET method, you tried: ${event.requestContext.http.method}`);
+    }
+
+    if(targeturl){
+        if(validUrl.isWebUri(targeturl)){
+            response = validateAMPWebsite(targeturl);
+            return  response
+        }
+        else {
+            response.statusCode = 400;
+            response.body = JSON.stringify({testResult: "Parameter targeturl is not a valid URL"});
+            return response
+        }
+
+    } else {
+        response.statusCode = 400;
+        response.body = JSON.stringify({testResult: "No targeturl parameter was found"});
+        return response;
+    }
+}
